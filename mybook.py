6d3693f4aaa644ee53f31d3cc03e6f35a4e8e187
Vladyslav Podrazhanskyi <49364023+VladyslavPodrazhanskyi@users.noreply.github.com>
2020-05-29 21:51:54 +0300

    initial commit

---
+++ mybook.py
@@ -0,0 +1,9 @@
+from tkinter import Tk, Button, Label, Scrollbar, Listbox, StringVar, Entry, N, S, W, E, END
+from tkinter import ttk
+from tkinter import messagebox
+
+# main window of the application
+root = Tk()
+
+# running the application
+root.mainloop()
