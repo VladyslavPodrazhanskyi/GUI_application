aa207da5189202bd184aae8b23c5084f2a682e4c
Vladyslav Podrazhanskyi <49364023+VladyslavPodrazhanskyi@users.noreply.github.com>
2020-05-31 22:35:27 +0300

    finished sql server gui application

--- mybook.py
+++ mybook.py
@@ -64,9 +64,70 @@ class Bookdb():
         self.con.commit()
         messagebox.showinfo(title='Book Database', message="Book Deleted")
 
+# Создаем экземпляр класса Bookdb.
+db = Bookdb()
+
+# Создание функций ( create a function for selected row inside Listbox)
+
+def get_selected_row(event):
+    '''This function shows title, author, isbn of the book
+    chosen in the row of list box
+    on the top panel of Gui'''
+    global selected_tuple
+    index = list_box.curselection()[0] # Метод curselection() позволяет получить в виде кортежа индексы выбранных элементов экземпляра Listbox.
+    selected_tuple = list_box.get(index) # Кортеж из индекс, названия, автор и isbn
+    title_entry.delete(0, 'end') # Очистка окна title
+    title_entry.insert('end', selected_tuple[1]) # вставка названия выбранной книги.
+    author_entry.delete(0, 'end')
+    author_entry.insert('end', selected_tuple[2])
+    isbn_entry.delete(0, 'end')
+    isbn_entry.insert('end', selected_tuple[3])
+
+
+def view_records():
+    list_box.delete(0, 'end')
+    for row in db.view():
+        list_box.insert('end', row)
+
+
+def add_book():
+    db.insert(title_text.get(), author_text.get(), isbn_text.get())
+    list_box.delete(0, 'end')
+    list_box.insert('end', (title_text.get(), author_text.get(), isbn_text.get()))
+    title_entry.delete(0, 'end')
+    author_entry.delete(0, 'end')
+    isbn_entry.delete(0, 'end')
+    con.commit()
+
+
+def delete_records():
+    db.delete(selected_tuple[0])
+    con.commit() # считаю, что не нужно, так как комит встроен в метод db.delete
+
+
+def clear_sreen():
+    list_box.delete(0, 'end')
+    title_entry.delete(0, 'end')
+    author_entry.delete(0, 'end')
+    isbn_entry.delete(0, 'end')
+
+
+def update_records():
+    db.update(selected_tuple[0], title_text.get(), author_text.get(), isbn_text.get())
+    title_entry.delete(0, 'end')
+    author_entry.delete(0, 'end')
+    isbn_entry.delete(0, 'end')
+    con.commit()
+
+def on_closing():
+    dd = db
+    if messagebox.askokcancel("Quit", "Do you want to quit?"):
+        root.destroy()
+        del dd
 
 # main window of the application
 root = Tk()
+
 
 root.title('My books database application')
 root.configure(background='light green')
@@ -103,7 +164,7 @@ isbn_entry = ttk.Entry(root, text=isbn_text)
 isbn_entry.grid(row=0, column=5, sticky=W)
 
 # 1.4.Button "Add Book"
-add_btn = Button(root, text='Add book', bg='dark blue', fg='white', font='helvetica 10 bold', command='')
+add_btn = Button(root, text='Add book', bg='dark blue', fg='white', font='helvetica 10 bold', command=add_book)
 add_btn.grid(row=0, column=6, sticky=W)
 
 # 2. ListBox:
@@ -111,6 +172,7 @@ add_btn.grid(row=0, column=6, sticky=W)
 # 2.1 listbox
 list_box = Listbox(root, height=16, width=40, font='helvetica 13', bg='light blue')
 list_box.grid(row=3, column=1, columnspan=14, sticky=W+E, padx=15, pady=40)
+list_box.bind('<<ListboxSelect>>', get_selected_row)
 
 # 2.2 Scroll bar:
 scroll = Scrollbar(root)
@@ -122,21 +184,23 @@ scroll.configure(command=list_box.yview)
 
 # 3. Bottom buttons ( 15 row  -  list box 1-14 rows)
 
-view_all_btn = Button(root, text='View all records', bg='black', fg='white', font='helvetica 10 bold', command="")
+view_all_btn = Button(root, text='View all records', bg='black', fg='white', font='helvetica 10 bold', command=view_records)
 view_all_btn.grid(row=15, column=1)
 
-clear_screen_btn = Button(root, text='Clear Screen', bg='dark red', fg='white', font='helvetica 10 bold', command="")
+clear_screen_btn = Button(root, text='Clear Screen', bg='dark red', fg='white', font='helvetica 10 bold', command=clear_sreen)
 clear_screen_btn.grid(row=15, column=2)
 
-exit_app_btn = Button(root, text='Exit application', bg='blue', fg='white', font='helvetica 10 bold', command="")
+exit_app_btn = Button(root, text='Exit application', bg='blue', fg='white', font='helvetica 10 bold', command=root.destroy)
 exit_app_btn.grid(row=15, column=3)
 
-modify_rec_btn = Button(root, text='Modify record', bg='purple', fg='white', font='helvetica 10 bold', command="")
+modify_rec_btn = Button(root, text='Modify record', bg='purple', fg='white', font='helvetica 10 bold', command=update_records)
 modify_rec_btn.grid(row=15, column=4)
 
-delete_rec_btn = Button(root, text='Delete record', bg='red', fg='white', font='helvetica 10 bold', command="")
+delete_rec_btn = Button(root, text='Delete record', bg='red', fg='white', font='helvetica 10 bold', command=delete_records)
 delete_rec_btn.grid(row=15, column=5)
 
 
+
 # running the application
-root.mainloop()
+if __name__ == '__main__':
+    root.mainloop()
